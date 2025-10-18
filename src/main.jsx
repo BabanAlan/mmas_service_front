4ff0eee09988f5d0d0dc10d2582013fb9a124f2b
Araik Voskanyan <araik20061211@gmail.com>
2025-10-18 06:53:59 +0300

    tg

--- src/main.jsx
+++ src/main.jsx
@@ -37,6 +37,12 @@ const initializeTelegramSDK = async () => {
           window.Telegram.WebApp.disableSwipeBack?.(true);
         } catch {}
       }
+      createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
     }
   } catch (error) {
     console.error("Initialize error:", error);
@@ -45,8 +51,3 @@ const initializeTelegramSDK = async () => {
 
 initializeTelegramSDK();
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
