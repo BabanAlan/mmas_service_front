6969e02601c48e24e934f1e36653000ec37ec1a2
Araik Voskanyan <araik20061211@gmail.com>
2025-10-18 06:01:34 +0300

    tg

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 
+import { init, miniApp } from '@telegram-apps/sdk';
+
+const initializeTelegramSDK = async () => {
+  try {
+    await init();
+
+    if (miniApp.ready.isAvailable()) {
+      await miniApp.ready();
+      console.log('Mini App ready');
+    }
+
+  } catch (error) {
+    console.error('Initialize error:', error);
+  }
+  window.disableVerticalSwipes;
+};
+
+initializeTelegramSDK();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
